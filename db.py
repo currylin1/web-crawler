32b32bd371fbc5eacac3cdac6b180af06fa84601
Python <141690879+currylin1@users.noreply.github.com>
2024-05-27 08:37:42 +0800

    資料庫連結
    flask_app 檔案，用以連接 pythonwhere 的資料庫

---
+++ db.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  6 19:42:06 2023
+
+@author: USER
+"""
+
+import mysql.connector
+
+conn = mysql.connector.connect(host='lin17799.mysql.pythonanywhere-services.com',user='lin17799',password='lIn17799)(#LL@@',database='lin17799$default',auth_plugin='mysql_native_password')
+
+cursor = conn.cursor()
